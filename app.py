ab6fd06c91b3dfdd86c271bbac52e8b3e392d08b
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-06-17 15:39:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import random
 import string
 import time
+import pymysql
 import threading
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room
